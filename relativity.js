483a9a2d5083986b4b1e7897e9c8ad150f8e644e
ninja542 <theroboticcyborg@gmail.com>
2017-12-07 11:56:11 -0500

    graph

--- relativity.js
+++ relativity.js
@@ -1,1 +1,9 @@
-d3.select("#graph").append("svg");
+var margin = { top: 20, right: 20, bottom: 20, left: 20 },
+    width = 960 - margin.left - margin.right,
+    height = 640 - margin.top - margin.bottom;
+
+var svg = d3.select('#graph').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
