c536a5e3d6d9b860657fed2eda512ccc5741ade2
ninja542 <theroboticcyborg@gmail.com>
2017-12-14 00:17:58 -0500

    animation stuffs

--- relativity.js
+++ relativity.js
@@ -1,3 +1,4 @@
+// set up graph
 var margin = { top: 20, right: 20, bottom: 20, left: 20 },
 		width = 640 - margin.left - margin.right,
 		height = 640 - margin.top - margin.bottom;
@@ -7,29 +8,18 @@ var svg = d3.select('#graph').append('svg')
 		.attr('height', height + margin.top + margin.bottom)
 	.append('g')
 		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+d3.selectAll("#transform").style("position", "relative").attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 var xScale = d3.scaleLinear().domain([-100, 100]).range([0, width]);
 var yScale = d3.scaleLinear().domain([100, -100]).range([0, height]);
 var xAxis = d3.axisBottom(xScale);
 var yAxis = d3.axisLeft(yScale);
-
-// margins for the animation stuffs
-var margin2 = { top: 10, right: 10, bottom: 10, left: 10 },
-		width2 = 640 - margin2.left - margin2.right,
-		height2 = 320 - margin2.top - margin2.bottom;
-var svg2 = d3.select("#lightspeed").append('svg')
-		.attr("width", width2 + margin2.left + margin2.right)
-		.attr("height", height2 + margin2.top + margin2.bottom)
-	.append('g')
-		.attr("transform", 'translate(' + margin2.left + "," + margin2.top + ')');
-var svg3 = d3.select("#observer").append('svg')
-		.attr("width", width2 + margin2.left + margin2.right)
-		.attr("height", height2 + margin2.top + margin2.bottom)
-	.append('g')
-		.attr("transform", 'translate(' + margin2.left + "," + margin2.top + ')');
-
 svg.append("g").call(xAxis).attr("transform", "translate(" + 0 + ", " + yScale(0) + ")");
 svg.append("g").call(yAxis).attr("transform", "translate(" + xScale(0) + ", " + 0 + ")");
 
+// line function
+var line = d3.line().x(function(d){ return d.x; }).y(function(d){ return d.y; });
+
+// points for the minkowski diagram
 var pointData = [
 	{x: 40, y: 20}
 ];
@@ -37,15 +27,11 @@ svg.selectAll("circle").data(pointData).enter().append("circle").attr("fill", "r
 var rectangleData = [
 
 ];
-
-var line = d3.line().x(function(d){ return d.x; }).y(function(d){ return d.y; });
-
 // constant light speed graph
 svg.append("path").attr("d", line([
 		{x: 0, y: height},
 		{x: width, y: 0}
 	])).attr("stroke", "yellow").attr("stroke-width", 1);
-
 // transformed graph
 // speed is in units of c
 var speed = 0.6;
@@ -58,9 +44,22 @@ svg.append("g").call(xAxis).attr("transform", "translate(" + 0 + ", " + yScale(0
 svg.append("g").call(yAxis).attr("transform", "translate("+xScale(0)+", "+ 0 + ") rotate ("+angle+", 0,"+yScale(0)+")")
 	.attr("class", "redAxis");
 
+// vue app
 var specialApp = new Vue({
 	el: "#everything",
+	data: {
+		speed: 0.6,
+	},
 	mounted: function(){
-		console.log("hi");
 	}
+});
+
+// animation testing sorta
+d3.select("#observetrain").style("position", "relative").style("left", "-360px");
+anime({
+	targets: "#observer #observetrain",
+	translateX: 1100,
+	duration: 10000,
+	delay: 1000,
+	easing: "linear"
 });
