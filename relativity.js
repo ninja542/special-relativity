6d7d0f99e437e380f1d0a68d644f32ae572a72fe
ninja542 <theroboticcyborg@gmail.com>
2018-02-20 11:19:06 -0500

    fixed axes

--- relativity.js
+++ relativity.js
@@ -41,19 +41,19 @@ var tempangle = Math.atan(tempspeed)*180/Math.PI;
 var tempanglerad = Math.atan(tempspeed);
 // for the lorentz transformation
 var tempgamma = 1/(Math.sqrt(1-Math.pow(tempspeed, 2))/1);
-var temptransformXScale = d3.scaleLinear().domain([0, 1/tempgamma]).range([0, width]);
+var temptransformXScale = d3.scaleLinear().domain([0, 1/tempgamma]).range([0, Math.abs(width/Math.cos(tempanglerad))]);
 var temptransformXAxis = d3.axisBottom(temptransformXScale);
 var temptransformYScale = d3.scaleLinear().domain([0, 1/tempgamma]).range([Math.abs(height/Math.cos(tempanglerad)), 0]);
 var temptransformYAxis = d3.axisLeft(temptransformYScale);
 // gridlines
-svgSelection.append("g").call(d3.axisBottom(temptransformXScale).ticks(20).tickSize(-height).tickFormat(""))
-	.attr("transform", "translate(" + 0 + ", " + temptransformYScale(0) + ") rotate ("+(tempangle)+","+(temptransformXScale(0))+","+ "0"+")")
+svgSelection.append("g").call(d3.axisBottom(temptransformXScale).ticks(20).tickSize(-height-200).tickFormat(""))
+	.attr("transform", "translate(" + 0 + ", " + yScale(0) + ") rotate ("+(tempangle)+","+(xScale(0))+","+"0"+")")
 	.attr("class", "gridx1");
 svgSelection.append("g").call(d3.axisBottom(temptransformXScale).ticks(20).tickSize(height).tickFormat(""))
 	.attr("transform", "translate(" + 0 + ", " + temptransformYScale(0) + ") rotate ("+(tempangle)+","+(temptransformXScale(0))+","+ "0"+")")
 	.attr("class", "gridx2");
-svgSelection.append("g").call(d3.axisLeft(temptransformYScale).ticks(20).tickSize(-width).tickFormat(""))
-	.attr("transform", "translate(" + temptransformXScale(0) + ", " + 0 + ") rotate ("+(-tempangle)+", 0,"+temptransformYScale(0)+")")
+svgSelection.append("g").call(d3.axisLeft(temptransformYScale).ticks(20).tickSize(-width-200).tickFormat(""))
+	.attr("transform", "translate(" + temptransformXScale(0) + ", " + (height-Math.abs(height/Math.cos(tempanglerad))) + ") rotate ("+(-tempangle)+", 0,"+temptransformYScale(0)+")")
 	.attr("class", "gridy1");
 svgSelection.append("g").call(d3.axisLeft(temptransformYScale).ticks(20).tickSize(width).tickFormat(""))
 	.attr("transform", "translate(" + temptransformXScale(0) + ", " + 0 + ") rotate ("+(-tempangle)+", 0,"+temptransformYScale(0)+")")
@@ -85,16 +85,17 @@ var specialApp = new Vue({
 	},
 	methods: {
 		update: function(){
+			let scalevar = this.transformYScale();
 			d3.select("#specialX").call(d3.axisBottom(this.transformXScale())).attr("transform", "translate(" + 0 + ", " + yScale(0) + ") rotate ("+(-this.angle)+",0,"+ "0"+")");
-			d3.select("#specialY").call(d3.axisLeft(this.transformYScale())).attr("transform", "translate(" + xScale(0) + ", " + 0 + ") rotate ("+this.angle+", 0,"+yScale(0)+")");
-			d3.select(".gridx1").call(d3.axisBottom(this.transformXScale()).ticks(20).tickSize(-height).tickFormat(""))
+			d3.select("#specialY").call(d3.axisLeft(this.transformYScale())).attr("transform", "translate(" + xScale(0) + ", " + (height-Math.abs(height/Math.cos(this.anglerad))) + ") rotate ("+this.angle+", 0,"+(scalevar(0))+")");
+			d3.select(".gridx1").call(d3.axisBottom(this.transformXScale()).ticks(20).tickSize(-height-200).tickFormat(""))
 				.attr("transform", "translate(" + 0 + ", " + yScale(0) + ") rotate ("+(this.angle)+","+(xScale(0))+","+ 0 +")");
 			d3.select(".gridx2").call(d3.axisBottom(this.transformXScale()).ticks(20).tickSize(height).tickFormat(""))
 				.attr("transform", "translate(" + 0 + ", " + yScale(0) + ") rotate ("+(this.angle)+","+(xScale(0))+","+ "0"+")");
-			d3.select(".gridy1").call(d3.axisLeft(this.transformYScale()).ticks(20).tickSize(-width).tickFormat(""))
-				.attr("transform", "translate(" + xScale(0) + ", " + 0 + ") rotate ("+(-this.angle)+", 0,"+yScale(0)+")");
+			d3.select(".gridy1").call(d3.axisLeft(this.transformYScale()).ticks(20).tickSize(-width-200).tickFormat(""))
+				.attr("transform", "translate(" + xScale(0) + ", " + (height-Math.abs(height/Math.cos(this.anglerad))) + ") rotate ("+(-this.angle)+", 0,"+scalevar(0)+")");
 			d3.select(".gridy2").call(d3.axisLeft(this.transformYScale()).ticks(20).tickSize(width).tickFormat(""))
-				.attr("transform", "translate(" + xScale(0) + ", " + 0 + ") rotate ("+(-this.angle)+", 0,"+yScale(0)+")");
+				.attr("transform", "translate(" + xScale(0) + ", " + (height-Math.abs(height/Math.cos(this.anglerad))) + ") rotate ("+(-this.angle)+", 0,"+scalevar(0)+")");
 			d3.select(".train").attr("d", line([
 				{x: xScale(0), y: yScale(0)},
 				{x: xScale(0), y: yScale(trainlength/this.gamma)},
@@ -107,10 +108,10 @@ var specialApp = new Vue({
 			data.exit().remove();
 		},
 		transformXScale: function(){
-			return d3.scaleLinear().domain([0, 1/this.gamma]).range([0, width]);
+			return d3.scaleLinear().domain([0, 1/this.gamma]).range([0, Math.abs(width/Math.cos(this.anglerad))]);
 		},
 		transformYScale: function(){
-			return d3.scaleLinear().domain([1/this.gamma, 0]).range([0, height/Math.cos(this.angle)]);
+			return d3.scaleLinear().domain([1/this.gamma, 0]).range([0, Math.abs(height/Math.cos(this.anglerad))]);
 		}
 	},
 	computed: {
@@ -153,7 +154,6 @@ var specialApp = new Vue({
 		}
 	}
 });
-d3.select("#transform").append("circle").attr("cx", xScale(0)).attr("cy", yScale(0)).attr("fill", "none").attr("stroke", "black").attr("r", temptransformYScale(0.15)).attr("stroke-width", 1);
 // animation testing sorta
 // TODO: name the animations
 function playAnimation(){
